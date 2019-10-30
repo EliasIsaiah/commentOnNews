9a77bccb868404878bc78cd975e0f676f10d3f38
Elias Roussos <eliasIsaiah@outlook.com>
2019-10-30 15:20:41 -0400

    -added a search button to the page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'typeface-roboto';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import DescriptionIcon from '@material-ui/icons/Description';
+import Icon from '@material-ui/core/Icon';
+import { Container } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Container>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Button variant="contained" color="primary" className="scrapeBtn" startIcon={<DescriptionIcon />}>Search</Button>
       </header>
+
+      </Container>
+
     </div>
   );
 }
